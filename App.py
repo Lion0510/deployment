4154f01605d4e42b12ec84d2848511873542c944
Lion0510 <115381295+Lion0510@users.noreply.github.com>
2024-12-13 08:06:51 +0700

    Update App.py

--- App.py
+++ App.py
@@ -33,7 +33,7 @@ body {
     justify-content: center;
     align-items: center;
     background-color: rgba(255, 255, 255, 0.8);  /* Warna latar belakang transparan untuk navigasi */
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
     padding: 10px;
     border-radius: 5px;
     margin: 20px auto;
