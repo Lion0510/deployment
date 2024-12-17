5b489f29c1f25620379cdba5a001d5e382ddb552
Lion0510 <115381295+Lion0510@users.noreply.github.com>
2024-12-17 23:27:09 +0700

    Update App.py

--- App.py
+++ App.py
@@ -196,7 +196,7 @@ st.markdown("""
     <img src="https://pbs.twimg.com/profile_images/1272461269136576512/Uw9AShxq_400x400.jpg" alt="Logo Fakultas Teknologi" class="logo">
 </div>
 <div class="header-box">
-    <h1>        Klasifikasi Suara Burung Sumatera 🦜        </h1>
+    <h1>        KicauNet🦜        </h1>
     <p>Identifikasi Burung Berdasarkan Suara Secara Otomatis</p>
 </div>
 """, unsafe_allow_html=True)
